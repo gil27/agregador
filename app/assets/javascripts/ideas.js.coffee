4a200a1b6010b63f03729646573536a024613185
Gil <gilgomesp@gmail.com>
2014-09-27 22:08:45 -0300

    ok

--- app/assets/javascripts/ideas.js.coffee
+++
@@ -1,4 +0,0 @@
-$ ->
-	$('.actions').on 'ajax:beforeSend', (event, xhr, settings) ->
-  	settings.url += ('?page=' + window.progressPage)
-
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,11 +2,6 @@ class IdeasController < ApplicationController
   before_action :authenticate_user!
   def index
   	@ideas = Idea.all.paginate(params[:page])
-
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 
   def new
