216af4704e3b333d54a420514f85482a95dcb1c9
Gil <gilgomesp@gmail.com>
2014-10-18 14:38:30 -0300

    Novo layout

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,5 @@
 class IdeasController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:execute, :about]
   def index
   	@ideas = Idea.all.order('created_at desc').paginate(params[:page])
 
