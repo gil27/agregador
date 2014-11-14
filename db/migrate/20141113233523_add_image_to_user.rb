1e3d877a36b0a7390802cdc5ab46942e8064013c
Gil <gilgomesp@gmail.com>
2014-11-13 22:28:50 -0200

    ;)

--- db/migrate/20141113233523_add_image_to_user.rb
+++ db/migrate/20141113233523_add_image_to_user.rb
@@ -1,5 +1,9 @@
 class AddImageToUser < ActiveRecord::Migration
   def change
     add_column :users, :image, :string, default: "profile/grey-lamp.png"
+
+    User.find_each do |user|
+    	user.update(image: "profile/grey-lamp.png")
+    end
   end
 end
